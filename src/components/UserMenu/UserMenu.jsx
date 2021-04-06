74409d10cf6ee116fbf42d45e430345623db4548
At0mUA <zatom89@gmail.com>
2021-04-06 16:49:06 +0300

    fix

--- src/components/UserMenu/UserMenu.jsx
+++ src/components/UserMenu/UserMenu.jsx
@@ -48,21 +48,3 @@ export default function UserMenu() {
     </div>
   );
 }
-
-// const UserMenu = ({ avatar, name, onLogout }) => (
-//   <div className={s.container}>
-//     <img src={userAvatar} alt="" width="32" className={s.avatar} />
-//     <span className={s.name}>Welcome, {name}</span>
-//     <button type="button" className={s.btn} onClick={onLogout}>
-//       Logout
-//     </button>
-//   </div>
-// );
-
-// const mapStateToProps = state => ({
-//   name: authSelectors.getUserName(state),
-// });
-
-// export default connect(mapStateToProps, { onLogout: authOperations.logOut })(
-//   UserMenu,
-// );
