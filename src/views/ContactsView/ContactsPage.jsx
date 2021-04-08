31f5fb2fb99300d6dfe39f8eb28d194561b1d916
Denys Kravchuk <63023273+luminousnow@users.noreply.github.com>
2021-04-08 23:53:41 +0300

    create Components

---
+++ src/views/ContactsView/ContactsPage.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import '../../css/common.css';
+import style from './ContactsPage.module.css';
+import teamList from '../../teamList.json';
+
+function ContactsPage() {
+  return <h2>Contacts page</h2>;
+}
+
+export default ContactsPage;
