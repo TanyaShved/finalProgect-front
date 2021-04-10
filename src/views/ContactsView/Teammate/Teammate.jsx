67a3a2779f6313743606749784c21287b2a8f962
Denys Kravchuk <63023273+luminousnow@users.noreply.github.com>
2021-04-11 00:19:41 +0300

    separated code

--- src/views/ContactsView/Teammate/Teammate.jsx
+++ src/views/ContactsView/Teammate/Teammate.jsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import '../../../css/common.css';
 import style from './Teammate.module.css';
-import { string } from 'prop-types';
+import { array, string } from 'prop-types';
+import SocialNet from './SocialNets/SocialNets';
 
 function Teammate({
   fullname,
@@ -9,28 +10,27 @@ function Teammate({
   photoMobile,
   photoTablet,
   photoPc,
-  github,
-  linkedin,
+  social,
 }) {
   return (
     <li className={style.member}>
       <picture>
         <source
-          srcset="
+          srcSet="
               ./images/our-masters/mobile/john-smith-418.jpg  1x,
             "
           media="(max-width: 767px)"
         />
 
         <source
-          srcset="
+          srcSet="
               ./images/our-masters/tablet/john-smith-450.jpg  1x,
             "
           media="(max-width: 1179px)"
         />
 
         <source
-          srcset="
+          srcSet="
               ./images/our-masters/desktop/john-smith-370.jpg  1x,
             "
           media="(min-width: 1280px)"
@@ -42,28 +42,9 @@ function Teammate({
       <h3 className={style.name}>{fullname}</h3>
       <p className={style.position}>{position}</p>
       <ul className={style.socialNets}>
-        <li className={style.net}>
-          <a
-            href={github}
-            aria-label="ссылка на GitHub"
-            className={style.netButton}
-          >
-            <svg className={style.netIcon} width="20" height="20">
-              <use href="./images/sprite.svg#github"></use>
-            </svg>
-          </a>
-        </li>
-        <li className={style.net}>
-          <a
-            href={linkedin}
-            aria-label="ссылка на LinkedIn"
-            className={style.netButton}
-          >
-            <svg className={style.netIcon} width="20" height="20">
-              <use href="./images/sprite.svg#icon-linkedin"></use>
-            </svg>
-          </a>
-        </li>
+        {social.map(({ title, link }) => (
+          <SocialNet key={title} title={title} link={link} />
+        ))}
       </ul>
     </li>
   );
@@ -73,10 +54,9 @@ export default Teammate;
 
 Teammate.propTypes = {
   fullname: string.isRequired,
-  position: string.isRequired,
+  position: array.isRequired,
   photoMobile: string.isRequired,
   photoTablet: string.isRequired,
   photoPc: string.isRequired,
-  github: string.isRequired,
-  linkedin: string.isRequired,
+  social: array.isRequired,
 };
