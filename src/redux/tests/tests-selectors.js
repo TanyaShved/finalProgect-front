dc5a8bed4112b8e8fbe82ce968077208f4d2db43
Yurii Konoval <yuriioleksandrovichkonoval@gmail.com>
2021-04-11 15:44:53 +0300

    data in resultsview and diagramm from store

--- src/redux/tests/tests-selectors.js
+++ src/redux/tests/tests-selectors.js
@@ -1,10 +1,12 @@
 const getQuestions = state => state.tests.questions;
 const getResults = state => state.tests.results;
 const getTestUrl = state => state.tests.testUrl;
+const getTestStatistics = state => state.tests.statistics;
 
 const selectors = {
   getQuestions,
   getResults,
   getTestUrl,
+  getTestStatistics,
 };
 export default selectors;
