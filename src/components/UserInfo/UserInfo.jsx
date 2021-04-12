10ce7957f05cd7b2b382b2f3dd617b4a0fc8c525
At0mUA <zatom89@gmail.com>
2021-04-12 23:54:07 +0300

    code refactoring. added userInfo

---
+++ src/components/UserInfo/UserInfo.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import { useSelector } from 'react-redux';
+
+import { authSelectors } from '../../redux/auth';
+
+import s from './UserInfo.module.css';
+
+export default function UserInfo() {
+  const name = useSelector(authSelectors.getUserName);
+
+  //   const name = 'User1'; // Временная переменная
+  const userAvatar = false; // Временная переменная
+
+  return (
+    <div className={s.userInfoMenu}>
+      {userAvatar ? (
+        <img
+          src={userAvatar}
+          alt="User avatar"
+          width="30"
+          height="30"
+          className={s.userAvatar}
+        />
+      ) : (
+        <span className={s.userAvatar}>{name[0]}</span>
+      )}
+
+      <span className={s.userName}> {name} </span>
+    </div>
+  );
+}
