060fbfe8541983ca89a7dab83f7a97d3a4361384
Denys Kravchuk <63023273+luminousnow@users.noreply.github.com>
2021-04-15 01:58:06 +0300

    refactored markup

--- src/views/ContactsView/ContactsPage.jsx
+++ src/views/ContactsView/ContactsPage.jsx
@@ -11,33 +11,15 @@ function ContactsPage() {
       <h1 className={style.title}>Our team</h1>
       <span className={style.line}></span>
       <ul className={style.team}>
-        {teamList.map(
-          ({
-            _id,
-            fullname,
-            position,
-            photoMobileX1,
-            photoTabletX1,
-            photoPcX1,
-            photoMobileX2,
-            photoTabletX2,
-            photoPcX2,
-            social,
-          }) => (
-            <Teammate
-              key={_id}
-              fullname={fullname}
-              position={position}
-              photoMobileX1={photoMobileX1}
-              photoTabletX1={photoTabletX1}
-              photoPcX1={photoPcX1}
-              photoMobileX2={photoMobileX2}
-              photoTabletX2={photoTabletX2}
-              photoPcX2={photoPcX2}
-              social={social}
-            />
-          ),
-        )}
+        {teamList.map(({ _id, fullname, position, photo, social }) => (
+          <Teammate
+            key={_id}
+            fullname={fullname}
+            position={position}
+            photo={photo}
+            social={social}
+          />
+        ))}
       </ul>
     </>
   );
